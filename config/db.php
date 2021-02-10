60015a812ade56c7c33ad02b71c573dd2b036890
Andrey Demidenko <worm2fed@gmail.com>
2021-02-10 18:47:48 +0300

    [add] docker configuration and howto

--- config/db.php
+++ config/db.php
@@ -4,13 +4,13 @@ $db = require __DIR__ . '/local/db.php';
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=' . DB_NAME,
+    'dsn' => 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
     'username' => DB_USER,
     'password' => DB_PASSWORD,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
--- modules/listing/assets/ListingAsset.php
+++ modules/listing/assets/ListingAsset.php
@@ -2,10 +2,10 @@
 
 namespace app\modules\listing\assets;
 
-use yii\web\AssetBundle;   
+use yii\web\AssetBundle;
 
 
-class ListingAsset extends AssetBundle 
+class ListingAsset extends AssetBundle
 {
     public $sourcePath = '@app/modules/listing/assets';
 
@@ -14,17 +14,15 @@ class ListingAsset extends AssetBundle
         $this->sourcePath = __DIR__;
         parent::init();
     }
-    
-    public $js = [
-    ];
-    
+
+    public $js = [];
+
     public $css = [
         'css/custom.css',
     ];
-    
+
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapPluginAsset',
     ];
-
 }
